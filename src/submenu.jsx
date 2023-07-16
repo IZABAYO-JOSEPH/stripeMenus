f5c2f3ca922615cb9c26b8086c114beb50e7dec3
Joseph Izabayo <joseph.izabayo@studentambassadors.com>
2023-07-17 00:22:30 +0200

    handled better approach onMouseLeave using Refs

--- src/submenu.jsx
+++ src/submenu.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import sublinks from "./data";
 import { useGlobalContext } from "./context";
+import { useRef } from "react";
 const Submenu = () => {
 
   const { pageId,setPageId } = useGlobalContext();
   const currentPage = sublinks.find((item) => item.pageId === pageId);
+  const submenuContainer = useRef(null);
+ 
   const handleMouseLeave =  (event)=>{
-   setPageId(null)
+
+
+    const submenu = submenuContainer.current
+    const {left,right,bottom} = submenu.getBoundingClientRect()
+    const {clientX, clientY} = event
+     if(clientX < left -1 || clientX>right-1 || clientY > bottom-1){
+      setPageId(null)
+     }
+ 
   }
   return (
-    <div className={currentPage?"submenu show-submenu":"submenu"} onMouseLeave={handleMouseLeave}>
+    <div
+     className={currentPage?"submenu show-submenu":"submenu"} 
+     onMouseLeave={handleMouseLeave}
+     ref={submenuContainer}
+     >
       <h5>{currentPage?.page}</h5>
       <div
         className="submenu-links"
