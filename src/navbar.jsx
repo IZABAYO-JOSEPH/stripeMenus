df7941deaace79c6eeb4c826a01f2f51e60b7db2
Joseph Izabayo <joseph.izabayo@studentambassadors.com>
2023-07-16 04:23:45 +0200

    handled hidding of submenu

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -3,9 +3,14 @@ import {FaBars} from "react-icons/fa"
 import { useGlobalContext } from './context'
 import NavLinks from './Navlinks'
 const Navbar = () => {
-    const {openSidebar} = useGlobalContext()
+    const {openSidebar,setPageId} = useGlobalContext()
+    const handlemenu = (e)=>{
+if(!e.target.classList.contains('nav-link')){
+  setPageId(null)
+}
+    }
   return (
-    <nav>
+    <nav onMouseOver={handlemenu}>
         <div className="nav-center">
             <h3 className="logo">CONTEXT</h3>
           <button className='toggle-btn' onClick={openSidebar}><FaBars /></button>
